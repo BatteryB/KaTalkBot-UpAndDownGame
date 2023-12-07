c780a288def11b207f8db958566db6377f85a0fb
BatteryB <130419015+BatteryB@users.noreply.github.com>
2023-12-08 01:00:07 +0900

    Update UpAndDownGame.js

--- UpAndDownGame.js
+++ UpAndDownGame.js
@@ -1,42 +1,44 @@
+const prefix = '!'; // 접두사 변경 선택 / Select Change Prefix
+
 let isStart = false;
 let answer = 0;
 let count = 0;
 let chance = 0;
 
 function response(room, msg, sender, isGroupChat, replier, imageDB, packageName) {
-    
+
     if (room == 'your room name') {  //채팅방 이름 수정 필수 / Modifying chat room name is required
-        
-        if(msg == "!업다운"){
+
+        if (msg == prefix + "업다운") {
             replier.reply(help());
         }
-    
+
         start(msg, replier);
-    
+
         isCorrectAnswer(msg, replier, sender);
-    
+
         giveUp(msg, replier, sender);
     }
-    
+
 }
 
-function start(msg, replier){
-    if (msg == "!업다운시작" && !isStart) {
+function start(msg, replier) {
+    if (msg == prefix + "업다운시작" && !isStart) {
         answer = Math.floor(Math.random() * 99) + 1;
         replier.reply("정답이 정해졌습니다(기회 5번)\n1~100");
         isStart = true;
         count = 0;
         chance = 7;
-    }else if (msg == "!업다운" && isStart) {
+    } else if (msg == prefix + "업다운" && isStart) {
         replier.reply("이미 게임이 진행중 입니다.");
     }
 }
 
-function isCorrectAnswer(msg, replier, sender){
+function isCorrectAnswer(msg, replier, sender) {
     if (isStart) {
-        if (msg.startsWith("!업다운정답 ")) {
+        if (msg.startsWith(prefix + "업다운정답 ")) {
             let msgAnswer = Number(msg.substr(7));
-        
+
             if (msgAnswer == answer) {
                 count++;
                 chance--;
@@ -44,7 +46,7 @@ function isCorrectAnswer(msg, replier, sender){
                 isStart = false;
                 count = 0;
                 chance = 7;
-            }else if (count == 6) {
+            } else if (count == 6) {
                 replier.reply("기회 7번을 모두 사용했습니다\n정답:" + answer);
                 replier.reply("GAME OVER");
                 isStart = false;
@@ -63,8 +65,8 @@ function isCorrectAnswer(msg, replier, sender){
     }
 }
 
-function giveUp(msg, replier, sender){
-    if (msg == "!업다운포기" && isStart) {
+function giveUp(msg, replier, sender) {
+    if (msg == prefix + "업다운포기" && isStart) {
         replier.reply(sender + " 님이 포기하셨습니다\n정답:" + answer);
         isStart = false;
         count = 0;
@@ -72,7 +74,7 @@ function giveUp(msg, replier, sender){
     }
 }
 
-function help(){
+function help() {
     let msg = '업다운 명령어\n\n';
     const help = [
         '!업다운시작',
